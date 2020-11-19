04bd931af657f50bfa37bd9e41facdc362b13f04
flaviomahoney <flavio@mahoneyt.com>
2020-11-19 14:49:31 -0300

    Added some dependencies to product and order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :requests
   has_many :products, through: :requests
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :requests
   has_many :orders, through: :requests
 end
