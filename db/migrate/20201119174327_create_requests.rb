af9451cb074789d610eee1a351f2b6f64155cd3c
flaviomahoney <flavio@mahoneyt.com>
2020-11-19 14:46:58 -0300

    Created Request model

---
+++ db/migrate/20201119174327_create_requests.rb
@@ -0,0 +1,11 @@
+class CreateRequests < ActiveRecord::Migration[6.0]
+  def change
+    create_table :requests do |t|
+      t.string :status
+      t.references :product, null: false, foreign_key: true
+      t.references :order, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_173132) do
+ActiveRecord::Schema.define(version: 2020_11_19_174327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 2020_11_19_173132) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
+  create_table "requests", force: :cascade do |t|
+    t.string "status"
+    t.bigint "product_id", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_requests_on_order_id"
+    t.index ["product_id"], name: "index_requests_on_product_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,4 +59,6 @@ ActiveRecord::Schema.define(version: 2020_11_19_173132) do
 
   add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
+  add_foreign_key "requests", "orders"
+  add_foreign_key "requests", "products"
 end
