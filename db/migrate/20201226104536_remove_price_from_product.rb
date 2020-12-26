f67bf38f668c5746fc93a0b7b675650871f92b9a
flaviomahoney <flavio@mahoneyt.com>
2020-12-26 08:09:46 -0300

    Removed Size from products and added in requests

--- db/migrate/20201226104536_remove_price_from_product.rb
+++
@@ -1,5 +0,0 @@
-class RemovePriceFromProduct < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :products, :price
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_26_104536) do
+ActiveRecord::Schema.define(version: 2020_12_26_110909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2020_12_26_104536) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "size"
+    t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_12_26_104536) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.string "size"
     t.index ["product_id"], name: "index_requests_on_product_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
