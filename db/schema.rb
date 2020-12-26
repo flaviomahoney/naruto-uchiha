3f21e6fa1427d88a61857c8e3f1106ca31cff7d3
flaviomahoney <flavio@mahoneyt.com>
2020-12-26 07:47:07 -0300

    Removed price from products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_18_131341) do
+ActiveRecord::Schema.define(version: 2020_12_26_104536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2020_12_18_131341) do
     t.string "name"
     t.string "description"
     t.string "size"
-    t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
