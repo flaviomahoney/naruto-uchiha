56708f9b4a05d3bc0891b55177a1da8e7c22348d
flaviomahoney <flavio@mahoneyt.com>
2020-12-27 18:52:00 -0300

    Adding some seeds to the project, having some problems with dependecies that I already deleted but will keep trying to solve it

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# <%= product.name %>
-# <%= product.description %>
-# <%= product.size %>
-# <%= product.price %>
+#Exemplo
+
+# viktor_photo = "https://avatars3.githubusercontent.com/u/63431873?s=460&u=1084b312c7083849885fae61cf7f4db400eecf7a&v=4"
+# file = URI.open(viktor_photo)
+# filename = File.basename(URI.parse(viktor_photo).path)
+# viktor_user = User.new(email: "vkaraujo00@gmail.com", password: '123456', username: "Viktor", role: "admin")
+# viktor_user.photo.attach(io: file, filename: filename)
+# viktor_user.save
+
+require 'open-uri'
+OpenURI::Buffer.send :remove_const, 'StringMax' if OpenURI::Buffer.const_defined?('StringMax')
+OpenURI::Buffer.const_set 'StringMax', 0
+
 puts "Destroying all"
 Product.destroy_all
 User.destroy_all
@@ -17,21 +26,41 @@ User.destroy_all
 
 puts "Starting"
 
-# first = Product.new( name: "Camisa Naruto", description: "Essa camisa de naruto é muito boa", size: "GG", price:"24,50", user: User.all.sample)
-# first.save
-
 User.create!(email: "flavio@gmail.com", password: "123456")
 
-second = Product.new( name: "Camisa Sasuke", description: "Essa camisa de sasuke é muito boa", size: "", price:"39,90", user: User.all.sample)
+konoha_photo = "https://res.cloudinary.com/dmcepupsl/image/upload/v1608302586/tlynmru5qsryl4qimy7wx2n73w52.jpg"
+file = URI.open(konoha_photo)
+filename = File.basename(URI.parse(konoha_photo).path)
+first = Product.new( name: "Konoha", description: "Essa camisa de konoha é muito boa", price:"39,90", user: User.all.sample)
+first.photo.attach(io: file, filename: filename)
+first.save
+
+sasuke_photo = "https://res.cloudinary.com/dmcepupsl/image/upload/v1608303412/orozd19xgo3nu4snxjld23l9914s.png"
+file = URI.open(sasuke_photo)
+filename = File.basename(URI.parse(sasuke_photo).path)
+second = Product.new( name: "Camisa Sasuke", description: "Essa camisa de sasuke é muito boa", price:"39,90", user: User.all.sample)
+second.photo.attach(io: file, filename: filename)
 second.save
 
-product3 = Product.new( name: "Camisa Sakura", description: "Essa camisa de sakura é muito boa", size: "", price:"42,90", user: User.all.sample)
+sakura_photo = "https://res.cloudinary.com/dmcepupsl/image/upload/v1608303397/p8681zjyiwqio18tilegjcv6qoop.png"
+file = URI.open(sakura_photo)
+filename = File.basename(URI.parse(sakura_photo).path)
+product3 = Product.new( name: "Camisa Sakura", description: "Essa camisa de sakura é muito boa", price:"42,90", user: User.all.sample)
+product3.photo.attach(io: file, filename: filename)
 product3.save
 
-product4 = Product.new( name: "Camisa Kakashi", description: "Essa camisa de kakashi é muito boa", size: "", price:"29,90", user: User.all.sample)
+kakashi_photo = "https://res.cloudinary.com/dmcepupsl/image/upload/v1608303445/4sm3p370kn0eyzabumdext0n533s.jpg"
+file = URI.open(kakashi_photo)
+filename = File.basename(URI.parse(kakashi_photo).path)
+product4 = Product.new( name: "Camisa Kakashi", description: "Essa camisa de kakashi é muito boa", price:"29,90", user: User.all.sample)
+product4.photo.attach(io: file, filename: filename)
 product4.save
 
-product5 = Product.new( name: "Camisa Tsunade", description: "Essa camisa de tsunade é muito boa", size: "", price:"29,90", user: User.all.sample)
+tsunade_photo = "https://res.cloudinary.com/dmcepupsl/image/upload/v1608303433/yc2egecj18oaqswktzwbkj4x9f2j.jpg"
+file = URI.open(tsunade_photo)
+filename = File.basename(URI.parse(tsunade_photo).path)
+product5 = Product.new( name: "Camisa Tsunade", description: "Essa camisa de tsunade é muito boa", price:"29,90", user: User.all.sample)
+product5.photo.attach(io: file, filename: filename)
 product5.save
 
 puts "Done"
